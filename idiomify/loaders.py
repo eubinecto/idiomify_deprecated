20c32162a9fbf32b8fa697e2067c9d63ebd2d834
Eu-Bin KIM <tlrndk123@gmail.com>
2021-03-26 12:28:03 +0900

    not storing the api key (at least not anymore). scraping with multiprocessing.

--- idiomify/loaders.py
+++ idiomify/loaders.py
@@ -1,11 +1,11 @@
-from typing import List, Dict
-from idiomify.paths import IDIOM_ALTS_TSV
+from typing import List, Dict, Tuple
+from idiomify.paths import IDIOM_ALTS_TSV, RAPID_API_TXT
 import csv
 import json
 
 
-def load_idiom_alts() -> Dict[str, List[str]]:
-    rows = dict()
+def load_idiom_alts() -> List[Tuple[str, List[str]]]:
+    rows = list()
     with open(IDIOM_ALTS_TSV, 'r') as fh:
         tsv_reader = csv.reader(fh, delimiter="\t")
         # skip the header
@@ -13,6 +13,10 @@ def load_idiom_alts() -> Dict[str, List[str]]:
         for row in tsv_reader:
             idiom = row[0]
             alts = json.loads(row[1])
-            rows[idiom] = alts
-
+            rows.append((idiom, alts))
     return rows
+
+
+def load_rapid_api_key() -> str:
+    with open(RAPID_API_TXT, 'r') as fh:
+        return fh.read().strip()
--- idiomify/paths.py
+++ idiomify/paths.py
@@ -4,10 +4,16 @@ from pathlib import Path
 LIB_DIR = Path(__file__).resolve().parent
 ROOT_DIR = LIB_DIR.parent
 DATA_DIR = ROOT_DIR.joinpath("data")
-DEFS_DIR = DATA_DIR.joinpath("defs"
-                             )
-# files
+DEFS_DIR = DATA_DIR.joinpath("defs")
+KEYS_DIR = DATA_DIR.joinpath("keys")
+
+
+# files - data
 IDIOM_ALTS_TSV = DATA_DIR.joinpath("idiom_alts.tsv")  # (idiom, alts list)
-URBAN_DEFS_RAW_TSV = DEFS_DIR.joinpath("urban_defs_raw.tsv")  # collection of raw data (idiom, resp)
-URBAN_DEFS_TSV = DEFS_DIR.joinpath("urban_defs.tsv")  # the raw definitions parsed to (idiom, defs)
-# and so on. (wiktionary, phrases, etc)
+
+# files - defs
+URBAN_IDIOM_RAWS_TSV = DEFS_DIR.joinpath("urban_idiom_raw.tsv")  # collection of raw data (idiom, resp)
+URBAN_IDIOM_DEFS_TSV = DEFS_DIR.joinpath("urban_idiom_defs.tsv")  # the raw definitions parsed to (idiom, defs)
+
+# files - keys
+RAPID_API_TXT = KEYS_DIR.joinpath("rapid_api.txt")
