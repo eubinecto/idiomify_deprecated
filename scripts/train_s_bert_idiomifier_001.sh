ace7f02d4f517a6f61009b7ce07fec987ca24154
Eu-Bin KIM <tlrndk123@gmail.com>
2021-04-15 10:10:22 +0900

    path typo fixed

--- scripts/train_s_bert_idiomifier_001.sh
+++ scripts/train_s_bert_idiomifier_001.sh
@@ -1,7 +1,7 @@
 # use the idiomify.train script.
 # this is the first iteration of idiomifier.
 
-python3 ../../idiomify/runners/train.py \
+python3 ../idiomify/runners/train.py \
   --bert_model_name="deepset/sentence_bert" \
   --bert_embed_size=768 \
   --idiom2vec_embed_size=200 \
@@ -10,5 +10,5 @@ python3 ../../idiomify/runners/train.py \
   --learning_rate=0.001 \
   --loss_fn="cosine_sim" \
   --num_workers=3 \
-  --def2embed_train_path="../../data/def2embed/def2embed_train.tsv" \
-  --save_path="../../data/idiomifier/s_bert_idiomifier_001.model"
+  --def2embed_train_path="../data/def2embed/def2embed_train.tsv" \
+  --save_path="../data/idiomifier/s_bert_idiomifier_001.model"
